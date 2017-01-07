80ebb38756e3a4929877ccdfb7c19b8bd00e2b2d
kimi <kimhimitsu@gmail.com>
2017-01-07 20:41:13 +0100

    fix colors on terminal

--- test/index.js
+++ test/index.js
@@ -2,12 +2,12 @@ var async = require('async');
 
 function handleResults(err, results) {
   if (err) {
-    console.error('\x1b[91m', err.log, '\x1b[0m', err.res, '\t');
+    console.error('\x1b[38;5;01m', err.log, '\x1b[0m', err.res, '\t');
     process.exit(1);
   }
   else {
     results.forEach(function(r) {
-      console.log('\x1b[92m✓\x1b[0m', '\x1b[90m', r.log, '\x1b[0m', r.res);
+      console.log('\x1b[38;5;02m✓\x1b[0m', '\x1b[38;5;08m', r.log, '\x1b[0m', r.res);
     });
     console.log('\t');
     this();
@@ -20,6 +20,6 @@ async.series(process.argv.slice(2).map(function(test) {
     async.map(proc.tests, proc.task, handleResults.bind(cb));
   };
 }), function() {
-  console.log('\x1b[92m', 'All tests done without any error', '\x1b[0m', '\n');
+  console.log('\x1b[38;5;02m', 'All tests done without any error', '\x1b[0m', '\n');
   process.exit();
 });
