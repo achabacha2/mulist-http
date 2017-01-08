b267fcd92a6c0c7dd7807b3637480ddd6d5b0c2a
kimi <kimhimitsu@gmail.com>
2017-01-08 02:41:45 +0100

    feat(config): auto-config for openshift v2 and heroku

--- config.js
+++ config.js
@@ -1,6 +1,4 @@
 module.exports = {
-	https: false,
-	port: process.env.PORT,
-	//host: '',
-	key: '',
+	port: process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 8080,
+	host: process.env.OPENSHIFT_NODEJS_IP || null,
 }
